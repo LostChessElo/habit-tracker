bd176ea75a180537d5f0e467ceb6bd60ba8e57bf
Luke Ferley <lukeferley6@gmail.com>
2026-06-01 09:47:07 +0200

    feat: add jwt authentication filtering.

---
+++ src/main/java/com/tracker/habit/auth/JwtAuthFilter.java
@@ -0,0 +1,49 @@
+package com.tracker.habit.auth;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtUtil util;
+
+    public JwtAuthFilter(JwtUtil util) {
+        this.util = util;
+    }
+
+    @Override
+    public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String header = request.getHeader("Authorization");
+
+        if (header == null || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // remove substring "bearer "
+        String token = header.substring(7).trim();
+
+        try {
+            Long uid = util.extractUserId(token);
+            UsernamePasswordAuthenticationToken auth =
+                    new UsernamePasswordAuthenticationToken(uid, null, List.of());
+            SecurityContextHolder.getContext().setAuthentication(auth);
+            filterChain.doFilter(request, response);
+        } catch (JwtException e) {
+            filterChain.doFilter(request, response);
+        }
+
+    }
+}
