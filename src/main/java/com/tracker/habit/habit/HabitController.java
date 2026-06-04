79a3084d2a02b1e9f5a85354448440e74a69ff03
Luke Ferley <lukeferley6@gmail.com>
2026-06-04 10:02:25 +0200

    feat: complete habitController

--- src/main/java/com/tracker/habit/habit/HabitController.java
+++ src/main/java/com/tracker/habit/habit/HabitController.java
@@ -4,6 +4,7 @@ import com.tracker.habit.habit.dtos.CreateHabitRequest;
 import com.tracker.habit.habit.dtos.HabitResponse;
 import com.tracker.habit.habit.dtos.UpdateHabitRequest;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -21,26 +22,42 @@ public class HabitController {
 
     @GetMapping
     public ResponseEntity<List<HabitResponse>> getAllHabits(Authentication auth) {
-
+        Long userId = (Long) auth.getPrincipal();
+        return ResponseEntity.ok(service.getAllHabits(userId));
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<HabitResponse> getHabit(Authentication auth, @PathVariable Long habitId) {
-
+        Long userId = (Long) auth.getPrincipal();
+        return ResponseEntity.ok(service.getHabit(habitId, userId));
     }
 
     @PostMapping
     public ResponseEntity<HabitResponse> createHabit(Authentication auth, @RequestBody @Valid CreateHabitRequest createHabitRequest) {
-
+        Long userId = (Long) auth.getPrincipal();
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(service.createHabit(
+                        createHabitRequest.name(),
+                        createHabitRequest.description(),
+                        userId
+                ));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<HabitResponse> updateHabit(Authentication auth, @PathVariable @RequestBody @Valid UpdateHabitRequest updateHabitRequest) {
-
+    public ResponseEntity<HabitResponse> updateHabit(Authentication auth, @PathVariable Long habitId, @RequestBody @Valid UpdateHabitRequest updateHabitRequest) {
+        Long userId = (Long) auth.getPrincipal();
+        return ResponseEntity.ok(service.updateHabit(
+                habitId,
+                userId,
+                updateHabitRequest.name(),
+                updateHabitRequest.description()
+        ));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteHabit(Authentication auth, @PathVariable Long habitId) {
-
+        Long userId = (Long) auth.getPrincipal();
+        service.deleteHabit(habitId,userId);
+        return ResponseEntity.noContent().build();
     }
 }
